d6bb4431693f1e31888b9bfe3c6c6060d040f710
Gabo Esquivel <contact@gaboesquivel.com>
2019-08-03 15:59:04 -0600

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import styled from '@emotion/styled'
 
-const App: React.FC = () => {
+const Blockmatic = styled.div`
+  color: #353535;
+  display: flex;
+  self-align: center;
+  justify-content: center;
+  height: 100vh;
+  width: 100vw;
+  align-items: center;
+  font-size: 10em;
+  letter-spacing: .07em;
+`
+
+export default function App () : JSX.Element {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Blockmatic>blockmatic</Blockmatic>
   );
 }
-
-export default App;
