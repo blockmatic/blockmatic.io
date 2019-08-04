1375ee3ee69271e7c27d69dba61fe408e4adec64
Gabo Esquivel <contact@gaboesquivel.com>
2019-08-04 11:30:31 -0600

    add animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useState, useEffect} from 'react'
 import styled from '@emotion/styled'
 import { FaGithubAlt, FaMediumM, FaTwitter, FaEnvelope } from 'react-icons/fa'
+import posed  from 'react-pose'
+import SplitText from 'react-pose-text'
 
 const LandingWrapper = styled.div`
   display: flex;
@@ -17,11 +19,15 @@ const Main = styled.div`
   justify-content: center;
 `
 
-const Blockmatic = styled.div`
+const Blockmatic = styled(posed.div({
+  hidden: { opacity: 0},
+  visible: { opacity: 1},
+}))`
   color: #353535;
   font-family: 'Dhurjati', Sans-Serif;
   font-size: 10em;
   letter-spacing: .06em;
+  transition: all 2s ease-in;
 `
 
 const Slogan = styled.div`
@@ -29,26 +35,53 @@ const Slogan = styled.div`
   font-size: calc(1vw + 2.5vh);;
 `
 
-const Social = styled.div`
+const Social = styled(posed.div({
+  hidden: { opacity: 0 },
+  visible: { opacity: 1, delay: 5000 }
+}))`
+  opacity: 0;
   font-size: calc(1vw + 2.5vh);
   display: flex;
   direction: row;
   justify-content: center;
+  transition: opacity .3s ease-in;
 `
 const SocialLink = styled.a`
   color: #353535;
   padding: 1em;
 `
 
+const charPoses = {
+  enter: { 
+    opacity: 1,
+    delay: ({ charIndex }:{charIndex:number}) => charIndex * 40
+  },
+  exit: { 
+    opacity: 0
+  }
+};
 
 export default function App () : JSX.Element {
+  const [visible, setVisible] = useState<boolean>(false)
+  const [sloganVisible, setSloganVisible] = useState<boolean>(false)
+  
+  useEffect(() => setVisible(true),[setVisible])
+  useEffect(() => {
+    const sloganTimeout = setTimeout(() => setSloganVisible(true), 3000)
+    return () => clearTimeout(sloganTimeout)
+  },[setSloganVisible])
+
   return (
     <LandingWrapper>
       <Main>
-        <Blockmatic>blockmatic</Blockmatic>
-        <Slogan>build on EOSIO</Slogan>
+        <Blockmatic pose={visible? 'visible': 'hidden'}>blockmatic</Blockmatic>
+        <Slogan>
+          <SplitText charPoses={charPoses} pose={sloganVisible ? 'enter' : 'exit'}>
+            build on EOSIO
+          </SplitText>
+        </Slogan>
       </Main>
-      <Social>
+      <Social pose={visible? 'visible': 'hidden'}>
         <SocialLink href='https://github.com/blockmatic' target='_blank'><FaGithubAlt /></SocialLink>
         <SocialLink href='https://medium.com/blockmatic' target='_blank'><FaMediumM /></SocialLink>
         <SocialLink href='https://twitter.com/blockmatic_io' target='_blank'><FaTwitter /></SocialLink>
